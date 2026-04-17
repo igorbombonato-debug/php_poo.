d5d347f08e6ec22a6a2b30340c1a2f802db28ebb
igor Bombonato <igor.bombonato@aluno.senai.br>
2026-04-17 08:43:57 -0300

    iguin

--- exemplo_carro.php
+++ exemplo_carro.php
@@ -9,20 +9,30 @@ class Carro {
         $this->velocidade = $velocidade;
     }
     public function getVelocidade (){
-        return $this->Velocidade;
+        return $this->velocidade;
     }
      public function getModelo (){
         return $this->modelo;
+    }
+
+    public function setVelocidade($novaVelocidade){
+        if($novaVelocidade >= 0 && $novaVelocidade < 201){
+            $this->velocidade = $novaVelocidade;
+        }else{
+            $this->velocidade='ERRO: VELOCIDADE IMPRÓPRIA';
+        }
     }
 }
 
 // --- TESTE DO VEÍCULO ---
 $meuCarro = new Carro("Senai-Mobile", 0);
 
-// O desastre: alteração direta sem validação
-$meuCarro->velocidade = 200; // Velocidade de foguete?
-$meuCarro->velocidade = 0;   // Carro andando no tempo?
+$meuCarro->setVelocidade (190);
 
-echo "Modelo: " . $meuCarro->modelo . "<br>";
-echo "Velocidade atual: " . $meuCarro->velocidade . " km/h";
+
+echo "Modelo: " . $meuCarro->getModelo() . "<br>";
+if($meuCarro->getVelocidade() == "ERRO:velocidade impropria") {
+    echo'' . $meuCarro->getVelocidade();
+}
+echo "Velocidade atual: " . $meuCarro->getVelocidade() . " km/h";
 ?>
